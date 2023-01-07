a57a037a29a4c4fee78a918ba6006b3b31c54b4e
taturl <taturl@github.com>
2023-01-07 21:48:35 +0200

    Add changing level

--- src/Scripts/Road.gd
+++ src/Scripts/Road.gd
@@ -1,16 +1,23 @@
 extends ParallaxBackground
 
+onready var dirt_sprite = preload("res://Sprites/dirt.png")
+onready var snow_sprite = preload("res://Sprites/snow.png")
+
 var can_scroll = true
 export var increase_accelleration = true
 var speed = 0
 var accelleration = 1
+var level = "snow"
 
 
 func _ready():
 	speed = get_node("..").speed
 
-#func _process(delta):
-#	if can_scroll:
-#		scroll_offset.x -= speed * delta * accelleration
-#		if increase_accelleration:
-#			accelleration += 0.1 * delta
+
+func change_level():
+	if level == "snow":
+		$"/root/Main/ParallaxBackground/Road/Sprite".set_texture(dirt_sprite)
+		level = "dirt"
+	else:
+		$"/root/Main/ParallaxBackground/Road/Sprite".set_texture(snow_sprite)
+		level = "snow"
--- src/Scripts/ScoreLabel.gd
+++ src/Scripts/ScoreLabel.gd
@@ -5,3 +5,6 @@ var score = 0
 func increment_score():
 	score += 1
 	text = "Forest raked: %s" % score
+
+	if score % 5 == 0:
+		$"/root/Main/ParallaxBackground".change_level()
